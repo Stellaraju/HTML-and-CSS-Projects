50e9d573383d1230c4b1c99b4734c7b741d592bc
Stellaraju <stellaraju2004@gmail.com>
2024-11-27 00:07:14 -0800

    HTML and JavaScript Files
    This is the assignment of HTML and JavaScript files

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("This is my alert method");
+
+function My_First_Function()
+{
+	var String = "Call me, I am Stella";
+	var result = String.fontcolor("green");
+	document.getElementById("stella").innetHTML=result;
+}
