6ecb79cb51134f250c4a39bb0a6280ddbc5251e5
Stellaraju <stellaraju2004@gmail.com>
2024-12-01 01:30:24 -0800

    ONE PAGE WEBSITE and HTML ASSIGNMENTS
    This is my One Page Website Assignment and HTML Assignments

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++
@@ -1,8 +0,0 @@
-alert("This is my alert method");
-
-function My_First_Function()
-{
-	var String = "Call me, I am Stella";
-	var result = String.fontcolor("green");
-	document.getElementById("stella").innetHTML=result;
-}
